f5207a4221359db234d8062bae76f9fa066c60d1
Theo Strahlen <theo.strahlen@hyperisland.se>
2012-01-18 23:37:05 +0100

    added db.sql

---
+++ db.sql
@@ -0,0 +1,47 @@
+CREATE TABLE `fgd` (
+  `aID` int(11) NOT NULL auto_increment,
+  `aText` text NOT NULL,
+  `aURL` varchar(10) NOT NULL,
+  `aAccepted` int(4) NOT NULL default '0',
+  PRIMARY KEY  (`aID`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=39 ;
+
+-- 
+-- Dumping data for table `fgd`
+-- 
+
+INSERT INTO `fgd` VALUES (1, 'Give some fucking feedback.', 'gOrR', 1);
+INSERT INTO `fgd` VALUES (2, 'Set a clear fucking goal.', 'bnM7', 1);
+INSERT INTO `fgd` VALUES (3, 'Be fucking honest.', 'kVLE', 1);
+INSERT INTO `fgd` VALUES (4, 'Speak your fucking mind.', 'D5E4', 1);
+INSERT INTO `fgd` VALUES (5, 'Everything is a fucking learning experience.', 'mACh', 1);
+INSERT INTO `fgd` VALUES (6, 'What can you fucking do better?', 'DGHJ', 1);
+INSERT INTO `fgd` VALUES (7, 'Create a fucking plan.', 'GlWm', 1);
+INSERT INTO `fgd` VALUES (8, 'Come up with a fucking solution.', 'CJ8I', 1);
+INSERT INTO `fgd` VALUES (9, 'Set some fucking goals.', 'nopp', 1);
+INSERT INTO `fgd` VALUES (10, 'We always keep fucking learning.', 'A1mJ', 1);
+INSERT INTO `fgd` VALUES (11, 'Share your fucking thoughts.', 'MWde', 1);
+INSERT INTO `fgd` VALUES (12, 'Conflicts are fucking healthy.', 'QH93', 1);
+INSERT INTO `fgd` VALUES (13, 'Share your fucking expectations.', '7A0A', 1);
+INSERT INTO `fgd` VALUES (14, 'You will always feel fucking lost at some point.', '7G0C', 1);
+INSERT INTO `fgd` VALUES (15, 'Trust your fucking team members.', 'QVOi', 1);
+INSERT INTO `fgd` VALUES (16, 'Limit your fucking emotional volatility.', 'vLRI', 1);
+INSERT INTO `fgd` VALUES (17, 'Insist of respectful fucking communication.', 'Y7Wo', 1);
+INSERT INTO `fgd` VALUES (18, 'Keep your fucking promises.', 'W9Xy', 1);
+INSERT INTO `fgd` VALUES (19, 'Help someone get closer to their fucking goal.', 'V2DB', 1);
+INSERT INTO `fgd` VALUES (20, 'Celebrate fucking achievements.', 'zBuw', 1);
+INSERT INTO `fgd` VALUES (21, 'See some fucking potential in differences.', '2tVX', 1);
+INSERT INTO `fgd` VALUES (22, 'Be fucking constructive.', 'H4O2', 1);
+INSERT INTO `fgd` VALUES (23, 'Do some fucking reflection.', 'Ewzs', 1);
+INSERT INTO `fgd` VALUES (24, 'Think of fucking things that you appreciate.', 'smGe', 1);
+INSERT INTO `fgd` VALUES (25, 'Tell someone how you fucking feel.', 'nUah', 1);
+INSERT INTO `fgd` VALUES (26, 'Be fucking proactive.', 'vHK4', 1);
+INSERT INTO `fgd` VALUES (27, 'Remember that most teams are in a fucking constant state of change.', 'sdlr', 1);
+INSERT INTO `fgd` VALUES (28, 'Choose your fucking words wisely.', 'DdLu', 1);
+INSERT INTO `fgd` VALUES (32, 'Test test test', 'B6RN', 2);
+INSERT INTO `fgd` VALUES (33, 'This should be pending', 'JJyg', 0);
+INSERT INTO `fgd` VALUES (34, 'Concentrate on the things you can fucking control.', 'KQIg', 1);
+INSERT INTO `fgd` VALUES (35, 'Be honest about what the fuck you want to achieve.', 'kIaP', 1);
+INSERT INTO `fgd` VALUES (36, 'Get out of your fucking comfort zone.', 'Wrfv', 1);
+INSERT INTO `fgd` VALUES (37, 'Take some fucking time to determine what is really bothering you.', 'NL7w', 1);
+INSERT INTO `fgd` VALUES (38, 'Be fucking nice to your leaders!', 'a298', 2);
